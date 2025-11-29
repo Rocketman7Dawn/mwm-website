c17f5140d332de5b2dec19cf859f6e7c6ff56f66
Cid Isbell <cidisbell@gmail.com>
2025-11-29 21:46:12 +0000

    Add emailAssist bridge module for email APIs

---
+++ lib/emailAssist.ts
@@ -0,0 +1,6 @@
+// lib/emailAssist.ts
+// Simple bridge module so imports from "@/lib/emailAssist"
+// can reuse the logic from "./emailIngest".
+
+export * from "./emailIngest";
+export { default } from "./emailIngest";
